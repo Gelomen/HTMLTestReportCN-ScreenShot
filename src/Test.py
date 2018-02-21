85fb35ac48a598e6512196b66e941aaa94c229ad
Gelomen <kai664792415@qq.com>
2018-02-21 16:48:23 +0800

    update

--- src/Test.py
+++ src/Test.py
@@ -21,16 +21,16 @@ class TestClass(unittest.TestCase):
         try:
             self.browser.find_element_by_xpath("//input[@id='kw']")
         except Exception:
-            file_name = self.daf.get_screen_shot(self.browser)
-            raise Exception(file_name)
+            img_name = self.daf.get_screen_shot(self.browser)
+            raise Exception(img_name)
 
     def test2_title(self):
         title = self.browser.title
         try:
             self.assertEqual(title, "百度一下，你就知道!", "Title不一致！")
         except AssertionError:
-            file_name = self.daf.get_screen_shot(self.browser)
-            raise AssertionError(file_name)
+            img_name = self.daf.get_screen_shot(self.browser)
+            raise AssertionError(img_name)
 
 
 if __name__ == "__main__":
